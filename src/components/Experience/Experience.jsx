cde2d3cc774c95e4554c37101c26c9ce8c8c3b85
Itay Hasid <itay080297@gmail.com>
2025-03-14 10:05:55 +0200

    edit history

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -30,7 +30,7 @@ const Experience = () => {
                 />
                 <div className={styles.historyItemDetails}>
                   <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
-                  <p>{`${historyItem.startDate}, ${historyItem.endDate}`}</p>
+                  <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
                   <ul>
                     {historyItem.experiences.map((experience, id) => {
                       return <li key={id}>{experience}</li>;
